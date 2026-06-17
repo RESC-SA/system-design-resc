089b897dc6076e954fd1a94e91a134be498f6fb6
Mahmoud Hafez Eltarqi <workspaccode@gmail.com>
2026-06-17 12:05:35 +0300

    added icon widget

---
+++ lib/src/components/icon_widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class IconWidget extends StatelessWidget {
+  final IconData icon;
+
+  final double? size;
+  final double? width;
+  final double? height;
+  final Color? color;
+  final double? weight;
+  final double? grade;
+  final double? opticalSize;
+  final String? semanticLabel;
+  final List<Shadow>? shadows;
+  final bool? applyTextScaling;
+  final double? fill;
+  final FontWeight? fontWeight;
+  final TextDirection? textDirection;
+  final bool? isShowContainerBackground;
+  final Color? containerBackgroundColor;
+  final BorderRadius? containerBorderRadius;
+  final List<BoxShadow>? containerBoxShadow;
+  //final bool? matchTextDirection;
+    
+  const IconWidget(
+      {super.key,
+      required this.icon,
+      this.size,
+      this.width,
+      this.height,
+      this.color,
+      this.weight,
+      this.grade,
+      this.opticalSize,
+      this.semanticLabel,
+      this.shadows,
+      this.applyTextScaling,
+      this.fill,
+      this.fontWeight,
+      this.textDirection,
+      this.isShowContainerBackground = false,
+      this.containerBackgroundColor,
+      this.containerBorderRadius,
+      this.containerBoxShadow,
+      // this.matchTextDirection
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return  isShowContainerBackground! ? Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: containerBackgroundColor ?? Colors.grey[200],
+        borderRadius: containerBorderRadius ?? BorderRadius.circular(8),
+        boxShadow: containerBoxShadow ?? [],
+      ),
+      child: Icon(
+        icon,
+        size: size,
+        color: color,
+        weight: weight,
+        fill: fill ?? 3,
+        textDirection: textDirection ?? TextDirection.ltr,
+        fontWeight: fontWeight ?? FontWeight.w400,
+        grade: grade ?? 0,
+        opticalSize: opticalSize ?? 24,
+        semanticLabel: semanticLabel ?? icon.toString(),
+        shadows: shadows ?? [],
+        applyTextScaling: applyTextScaling ?? false,
+        // matchTextDirection: matchTextDirection ?? false,
+      ),
+    ) : Icon(
+      icon,
+      size: size,
+      color: color,
+      weight: weight,
+      fill: fill ?? 3,
+      textDirection: textDirection ?? TextDirection.ltr,
+      fontWeight: fontWeight ?? FontWeight.w400,
+      grade: grade ?? 0,
+      opticalSize: opticalSize ?? 24,
+      semanticLabel: semanticLabel ?? icon.toString(),
+      shadows: shadows ?? [],
+      applyTextScaling: applyTextScaling ?? false,
+      // matchTextDirection: matchTextDirection ?? false,
+    );
+  }
+}
