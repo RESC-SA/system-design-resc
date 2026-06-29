30c1e70197d8afb602b79064d94cb28f7c92976e
Mahmoud Hafez Eltarqi <workspaccode@gmail.com>
2026-06-29 12:00:47 +0300

    new feature for svg animated

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -2,5 +2,9 @@
 class Assets {
   Assets._();
 
+  static const String svgChart = 'assets/svg/chart.svg';
   static const String svgHomesvg = 'assets/svg/homeSvg.svg';
+  static const String svgInfo = 'assets/svg/info.svg';
+  static const String svgLogoLoadingx8 = 'assets/svg/logo-loadingx8.svg';
+  static const String svgLogoLoadingxx8 = 'assets/svg/logo-loadingxx8.svg';
 }
